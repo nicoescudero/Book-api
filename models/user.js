a0987dc6caed584a74350fb56906c30883b60b7c
nicolas <nicolas.escudero00@gmail.com>
2021-08-07 18:10:00 -0300

    renaming the user path

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, type) => {
-    return sequelize.define('usuario', {
+    return sequelize.define('user', {
         dni: {
             primaryKey: true,
             type: type.INTEGER
