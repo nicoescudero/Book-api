a1f00b0e3cf59c3d85c5c424511803387d6bd330
nicolas <nicolas.escudero00@gmail.com>
2021-08-21 19:08:05 -0300

    added documentation of book

--- router/bookRoutes.js
+++ router/bookRoutes.js
@@ -1,47 +1,353 @@
 const { Router } = require('express');
 const route = Router();
 const { Book } = require('../configuration/database')
+const { check, validationResult } = require('express-validator');
 
-//get all books
+/**
+ * @swagger
+ * components:
+ *  securitySchemes:
+ *   authToken:
+ *    type: http
+ *    scheme: bearer
+ *    bearerFormat: JWT
+ *  schemas:
+ *   Book:
+ *    type: object
+ *    properties:
+ *     id:
+ *      type: integer
+ *      description: id generated automatically
+ *     name:
+ *      type: string
+ *      description: name of book
+ *     author:
+ *      type: string
+ *      description: name of the book's author
+ *    example:
+ *      id: 1
+ *      title: "Don Quijote de la Mancha"
+ *      author: "Miguel de Cervantes"
+ *   BookRequirements:
+ *    type: object
+ *    example:
+ *      name: ""
+ *      author: ""
+ *  parameters:
+ *   bookId:
+ *    in: path
+ *    name: bookId
+ *    required: true
+ *    description: id of book
+ *    schema:
+ *     type: integer
+ *     example: 9
+ *   bookName:
+ *    in: path
+ *    name: bookName
+ *    required: true
+ *    description: name of the book
+ *    schema:
+ *     type: string
+ *   bookAuthor:
+ *    in: path
+ *    name: bookAuthor
+ *    required: true
+ *    description: name of the author
+ *    schema:
+ *     type: string"
+ *  responses:
+ *   200:
+ *    description: Succes Operation
+ *    content:
+ *     application/json:
+ *      schema:
+ *       $ref: "#components/schemas/Book"
+ *   404:
+ *    description: Not found books
+ *    content:
+ *     application/json:
+ *      schema:
+ *         $ref: "#components/schemas/Book"
+ *   401:
+ *    description: Unauthorized Error
+ *    content:
+ *     application/json:
+ *      schema:
+ *        type: object
+ *        properties:
+ *         message:
+ *          type: string
+ *        example:    
+ *         message: Token not provided
+ * tags:
+ *  name: Book
+ *  description: Book Data, you need a token
+ */
+
+/**
+ * @swagger
+ * /book:
+ *  get:
+ *   security:
+ *    - authToken: []
+ *   summary: Get list of books
+ *   tags: [Book]
+ *   responses:
+ *    $ref: "#components/responses"
+ */
 route.get('/', async (req, res) => {
     const books = await Book.findAll();
-    res.json({ Succes: books })
-});
+    (books) ?
+        res.json({ Succes: books }) :
+        res.status(404).json({ message: 'Books not found' })
 
-//get book by id 
+});
+/**
+ * @swagger
+ * /book/{bookId}:
+ *  get:
+ *   summary: Get a book by Id
+ *   tags: [Book]
+ *   parameters:
+ *    - $ref: "#components/parameters/bookId"
+ *   security:
+ *    - authToken: []
+ *   responses:
+ *    $ref: "#components/responses"
+ */
 route.get('/:bookId', async (req, res) => {
     const book = await Book.findOne({ where: { id: req.params.bookId } });
-    res.json({ Succes: book });
+    (book) ?
+        res.json({ Succes: book }) :
+        res.status(404).json({ message: 'Book not found' });
 })
 
-//get book by name
+/**
+ * @swagger
+ * /book/name/{bookName}:
+ *  get:
+ *   summary: Get a book by Name
+ *   tags: [Book]
+ *   parameters:
+ *    - $ref: "#components/parameters/bookName"
+ *   security:
+ *    - authToken: []
+ *   responses:
+ *    $ref: "#components/responses"
+ */
 route.get('/name/:bookName', async (req, res) => {
     const book = await Book.findOne({ where: { name: req.params.bookName } });
-    res.json({ Succes: book });
+    (book) ?
+        res.json({ Succes: book }) :
+        res.status(404).json({ message: 'Book not found' });
 })
 
-//get book by author
+/**
+ * @swagger
+ * /book/author/{bookAuthor}:
+ *  get:
+ *   summary: Get books by name of the author
+ *   tags: [Book]
+ *   parameters:
+ *    - $ref: "#components/parameters/bookAuthor"
+ *   security:
+ *    - authToken: []
+ *   responses:
+ *    $ref: "#components/responses"
+ */
+
 route.get('/author/:bookAuthor', async (req, res) => {
-    const book = await Book.findOne({ where: { author: req.params.bookAuthor } });
-    res.json({ Succes: book });
+    const books = await Book.findAll({ where: { author: req.params.bookAuthor } });
+    (books) ?
+        res.json({ Succes: books }) :
+        res.status(404).json({ message: 'Not found author' });
 })
 
-//post book
-route.post('/', async (req, res) => {
-    const newBook = await Book.create(req.body);
-    res.json({ Succes: newBook })
+/**
+ * @swagger
+ * /book/:
+ *  post:
+ *   summary: Add a book
+ *   tags: [Book]
+ *   security:
+ *    - authToken: []
+ *   requestBody:
+ *    required: true
+ *    content:
+ *     application/json:
+ *      schema:
+ *       $ref: "#components/schemas/BookRequirements"
+ *   responses:
+ *    201:
+ *      description: Added book
+ *      content:
+ *       application/json:
+ *        schema:
+ *         type: object
+ *         example:
+ *          message: Added book
+ *    400:
+ *     description: message when some aspect of the request is invalid
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: "#components/schemas/InvalidRequest"
+ *    401:
+ *     description: Unauthorized Error
+ *     content:
+ *      application/json:
+ *       schema:
+ *         type: object
+ *         properties:
+ *           message:
+ *            type: string
+ *         example:
+ *           message: Token not provided
+ *    422:
+ *      description: Unprocessable Entity
+ *      content:
+ *       application/json:
+ *          schema:
+ *           type: object
+ *           properties:
+ *            message:
+ *             type: string
+ *          example:
+ *           message: Undocumented
+ */
+route.post('/', [
+    check('name', ' Name of book is required').notEmpty(),
+    check('author', 'Name of author is required').notEmpty(),
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() })
+    }
+    if (Object.keys(req.body).length < 3) {
+        const newBook = await Book.create(req.body);
+        res.status(201).json({ Succes: 'Added Book', book: newBook })
+    } else
+        res.status(400).json({ message: 'Error, Invalid Request' })
+
 });
 
-//delete book by id
-route.delete('/:bookId', async (req, res) => {
-    await Book.destroy({ where: { id: req.params.bookId } })
-    res.json({ Succes: 'Deleted' });
+/**
+ *  @swagger
+ * /book/{bookId}:
+ *  put:
+ *   summary: Update a book
+ *   tags: [Book]
+ *   security:
+ *    - authToken: []
+ *   parameters:
+ *    - $ref: "#components/parameters/bookId"
+ *   requestBody:
+ *    required: true
+ *    content:
+ *     application/json:
+ *      schema:
+ *       $ref: "#components/schemas/BookRequirements"
+ *   responses:
+ *    201:
+ *      description: Updated book
+ *      content:
+ *       application/json:
+ *        schema:
+ *         type: object
+ *         example:
+ *          message: Updated book
+ *    400:
+ *     description: message when some aspect of the request is invalid
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: "#components/schemas/InvalidRequest"
+ *    401:
+ *     description: Unauthorized Error
+ *     content:
+ *      application/json:
+ *       schema:
+ *         type: object
+ *         properties:
+ *           message:
+ *            type: string
+ *         example:
+ *           message: Token not provided
+ *    422:
+ *      description: Unprocessable Entity
+ *      content:
+ *       application/json:
+ *          schema:
+ *           type: object
+ *           properties:
+ *            message:
+ *             type: string
+ *          example:
+ *           message: Undocumented
+ */
+route.put('/:bookId', [
+    check('name', ' Name of book is required').notEmpty(),
+    check('author', 'Name of author is required').notEmpty(),
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() })
+    }
+    if (Object.keys(req.body).length < 3) {
+        await Book.update(req.body, { where: { id: req.params.bookId } })
+        res.status(201).json({ Succes: 'Updated Book' });
+    } else
+        res.status(400).json({ message: 'Error, Invalid Request' })
 })
 
-//update by id
-route.put('/:bookId', async (req, res) => {
-    await Book.update(req.body, { where: { id: req.params.bookId } })
-    res.json({ Succes: 'Updated Book' });
+/**
+ * @swagger
+ * /book/{bookId}:
+ *  delete:
+ *   summary: Delete a book
+ *   tags: [Book]
+ *   security:
+ *    - authToken: []
+ *   parameters:
+ *    - $ref: "#components/parameters/bookId"
+ *   responses:
+ *    200:
+ *      description:
+ *      content:
+ *       application/json:
+ *        schema:
+ *          type: object
+ *          example:
+ *           message: Book deleted
+ *    401:
+ *       description: Unauthorized Error
+ *       content:
+ *        application/json:
+ *         schema:
+ *           type: object
+ *           properties:
+ *            message:
+ *             type: string
+ *           example:
+ *            message: Token not provided
+ *    404:
+ *      description: Book not found
+ *      content:
+ *       application/json:
+ *        schema:
+ *         type: object
+ *        example:
+ *         message: Book not found  
+ */
+route.delete('/:bookId', async (req, res) => {
+    const book = await Book.findOne({ where: { id: req.params.bookId } });
+    if (book) {
+        await Book.destroy({ where: { id: req.params.bookId } })
+        res.json({ Succes: 'Deleted' });
+    } else
+        res.status(404).json({ message: 'Book not found' });
 })
+
 
 module.exports = route;
--- router/middleware.js
+++ router/middleware.js
@@ -1,22 +1,20 @@
 const jwt = require('jwt-simple');
 const moment = require('moment');
 
-const checkToken = (req, res, next) => {
-    if (!req.headers['x-access-token'])
-        return res.json({ message: 'You need a token' });
-
-    const userToken = {};
+const checkToken = async (req, res, next) => {
+    const authHeader = req.get('authorization');
+    const token = authHeader && authHeader.split(' ')[1];
+    var userToken = {};
+    if (token === null)
+        return res.status(401).json({ message: 'Need a token' });
     try {
-        userToken = jwt.decode(req.headers['x-access-token'], process.env.KEY);
+        userToken = jwt.decode(token, process.env.KEY);
     } catch (error) {
-        res.json({ message: 'The token is invalid' })
+        return res.status(401).json({ message: 'The token is invalid' })
     }
-
     if (userToken.expiredAt < moment.unix())
-        return res.json({ message: 'The token has expired' });
-
+        return res.status(401).json({ message: 'The token has expired' });
     req.userId = userToken.id;
-
     next();
 }
 
