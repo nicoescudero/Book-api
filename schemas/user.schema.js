eb2f58bd70676782fe80a79eb10a7484c7341b19
nicoescudero <nicolas.escudero00@gmail.com>
2022-12-25 23:07:21 -0300

    update: user validation scheme

--- schemas/user.schema.js
+++ schemas/user.schema.js
@@ -1,4 +1,4 @@
-module.exports = {
+exports.registerSchema = {
   name: {
     isString: {
       bail: true,
@@ -18,8 +18,76 @@ module.exports = {
       bail: true,
       errorMessage: 'Invalid Email',
     },
+    normalizeEmail: {
+      bail: true,
+    },
+    notEmpty: {
+      bail: true,
+      errorMessage: 'email must not be empty',
+    },
   },
   password: {
+    isLength: {
+      errorMessage: 'Password should be at least 8 chars long',
+      options: { min: 8 },
+    },
+  },
+};
+
+exports.loginSchema = {
+  email: {
+    isEmail: {
+      bail: true,
+      errorMessage: 'Invalid Email',
+    },
+    notEmpty: {
+      bail: true,
+      errorMessage: 'email must not be empty',
+    },
+  },
+  password: {
+    isLength: {
+      errorMessage: 'Password should be at least 8 chars long',
+      options: { min: 8 },
+    },
+  },
+};
+
+exports.putSchema = {
+  name: {
+    isString: {
+      bail: true,
+      errorMessage: 'Name is wrong',
+    },
+    isLength: {
+      errorMessage: 'Name should be at least 2 chars long',
+      options: { min: 2 },
+    },
+    isUppercase: {
+      negated: true,
+      errorMessage: 'Name should be in lower case',
+    },
+  },
+  email: {
+    isEmail: {
+      bail: true,
+      errorMessage: 'Invalid Email',
+    },
+    normalizeEmail: {
+      bail: true,
+    },
+    notEmpty: {
+      bail: true,
+      errorMessage: 'email must not be empty',
+    },
+  },
+  password: {
+    isLength: {
+      errorMessage: 'Password should be at least 8 chars long',
+      options: { min: 8 },
+    },
+  },
+  newPassword: {
     isLength: {
       errorMessage: 'Password should be at least 8 chars long',
       options: { min: 8 },
