21d99b79818e47d2374a0f89f63d466f3eaf8792
nicoescudero <nicolas.escudero00@gmail.com>
2022-12-25 22:48:35 -0300

    fix: rolevalidator

--- middlewares/roleValidator.js
+++ middlewares/roleValidator.js
@@ -3,8 +3,8 @@ const createHttpError = require('http-errors');
 exports.isAdmin = (req, res, next) => {
   try {
     if (req.roleId === 1) return next();
-    return createHttpError(403, '[You do not have permission to make the request]');
+    return next(createHttpError(403, '[You do not have permission to make the request]'));
   } catch (error) {
-    return createHttpError(403, `[You do not have permission to make the request] ${error.message}`);
+    return next(createHttpError(403, `[You do not have permission to make the request] ${error.message}`));
   }
 };
